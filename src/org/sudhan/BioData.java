514e39753471f8d73741ef1fc4023eec24be7f92
Sudhan1426 <sudhanv94@gmail.com>
2021-07-02 19:27:56 +0530

    Task Completed

---
+++ src/org/sudhan/BioData.java
@@ -0,0 +1,84 @@
+package org.sudhan;
+
+public class BioData {
+public static void main(String[] args) {
+    
+	byte age = 20;
+	System.out.println("My Age is :"+"\t"+age);
+	
+	short dob = 1994;
+	System.out.println("My Dob is :"+"\t"+dob);
+	
+	int pincode = 630551;
+	System.out.println("My Area Pincode is :"+"\t"+ pincode);
+	
+	long phNo = 7373796742l;
+	System.out.println("My phNo is :"+"\t"+phNo);
+	
+	float avg = 71.12345678f;
+	System.out.println("My Btech avg is :"+"\t"+avg);
+	
+	double salary = 36345.1234456;
+	System.out.println("My Salary is :"+"\t"+salary);
+	
+	char initial ='v';
+	char FirstLetter ='s';
+	System.out.println("My intial is :"+"\t"+initial);
+	System.out.println("My FirstLetter is :"+"\t"+FirstLetter);
+	
+	boolean isPerson = true;
+	System.out.println("Wheather the person is male or not :"+"\t"+isPerson);
+	
+    String emailId=("sudhan123@gmail.com");
+    System.out.println("my mail id is :"+emailId);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
